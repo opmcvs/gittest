3dacdeebb5e359ae1c1b341bdb986b9ffe60e07a
opmcvs <opmcvs@gmail.com>
2024-01-24 16:42:14 +0800

    amended1/30

---
+++ gittest.py
@@ -0,0 +1,8 @@
+df1 = pd.read_csv(r'C:\Users\HP\Downloads\LOTR.csv')
+df2 = pd.read_csv(r'C:\Users\HP\Downloads\LOTR 2.csv')
+
+# this is a test
+
+# this part is new
+newname = input("enter name")
+#Entered new info for git
